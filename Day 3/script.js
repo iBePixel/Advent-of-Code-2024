5e87d88f987f1fa0b3c770a2cfa4009f2e1da45b
Elliott Lamb <elliottlamb123@gmail.com>
2024-12-04 22:30:55 +0000

    first try too low

--- Day 3/script.js
+++ Day 3/script.js
@@ -6,5 +6,23 @@ let sample = input.toString()
 
 let array = [...sample.matchAll(/mul\x28[0-9]{1,3}\x2c[0-9]{1,3}\x29/gm)]
 
-console.log(array[100][0]);
+let test = array.map( x => ([...x]
+))
+console.log(test);
 
+let total = 0;
+
+const calculate = test.reduce((acc, val) => {
+
+  let num1 = val[0].slice(4, val[0].indexOf(','))
+  let num2 = val[0].slice(val[0].indexOf(',') + 1, val[0].indexOf(')'))
+  console.log(num1, num2)
+  console.log(val[0].indexOf(','))
+
+  total += num1 * num2;
+})
+
+
+calculate
+
+console.log(total)
