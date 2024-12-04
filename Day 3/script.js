e1b237454c7d827532b117821e7a0c935ead3cea
Elliott Lamb <elliottlamb123@gmail.com>
2024-12-04 21:03:00 +0000

    Formatting and returning values individually

--- Day 3/script.js
+++ Day 3/script.js
@@ -1,13 +1,10 @@
+
 const fs = require("fs");
 
-const input = fs.readFile("input.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    let array = data.matchAll(/ mul\x28[0-9]{1,3}\x2c[0-9]{1,3}\x29 /gm)
-    console.log(array);
-    //console.log(data);
-  }
-});
+const input = fs.readFileSync("input.txt", )
+let sample = input.toString()
 
-input;
+let array = [...sample.matchAll(/mul\x28[0-9]{1,3}\x2c[0-9]{1,3}\x29/gm)]
+
+console.log(array[100][0]);
+
