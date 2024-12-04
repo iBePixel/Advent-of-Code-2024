326e8cd4437d2b39fdf05580aa72c83c2d2ce5ea
Elliott Lamb <elliottlamb123@gmail.com>
2024-12-04 22:50:42 +0000

    Fixed bug, achieved star 1

--- Day 3/script.js
+++ Day 3/script.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const input = fs.readFileSync("input.txt", )
 let sample = input.toString()
 
-let array = [...sample.matchAll(/mul\x28[0-9]{1,3}\x2c[0-9]{1,3}\x29/gm)]
+let array = [...sample.matchAll(/mul\x28[0-9]{1,3}\x2c[0-9]{1,3}\x29/igm)]
 
 let test = array.map( x => ([...x]
 ))
@@ -12,14 +12,17 @@ console.log(test);
 
 let total = 0;
 
-const calculate = test.reduce((acc, val) => {
+const calculate = test.map((val) => {
 
   let num1 = val[0].slice(4, val[0].indexOf(','))
   let num2 = val[0].slice(val[0].indexOf(',') + 1, val[0].indexOf(')'))
-  console.log(num1, num2)
-  console.log(val[0].indexOf(','))
 
-  total += num1 * num2;
+  num1 = parseInt(num1);
+  num2 = parseInt(num2)
+  console.log(num1, num2)
+  
+  total += (num1 * num2);
+  //console.log(total)
 })
 
 
