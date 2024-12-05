ae102dd149d085d26697692b07eedfd42d3e18a2
Elliott Lamb <elliottlamb123@gmail.com>
2024-12-05 22:42:19 +0000

    Indexed arrays

--- Day 4/script.js
+++ Day 4/script.js
@@ -4,6 +4,10 @@ let sample = input.toString()
 let split = sample.split(/\r?\n/g);
 
 let counter = 0;
+let letterX = []
+let letterM = []
+let letterA = []
+let letterS = []
 
 split.forEach(line => {  
     matches = line.match(/XMAS|SAMX/g)
@@ -15,16 +19,21 @@ split.forEach(line => {
 for (let i = 0; i < split.length; i++){
     let letters = split[i].split('')
     
-    for (let a = 0; a < letters.length; a++){
-        if(letters[a] == 'X'){
-    }}
-    letters.forEach(letter => {
-        if (letter == 'X') {
-            let M = split[i+1][i]
-            //console.log(letter);
+    for (let f = 0; f < letters.length + 1; f++){
+        if(letters[f] === 'X'){
+            letterX.push([i,f])
         }
-    })
+        else if(letters[f] === 'M'){
+            letterM.push([i.f])
+        }
+        else if(letters[f] === 'A') {
+            letterA.push([i,f])
+        }
+        else if(letters[f] === 'S') {
+            letterS.push([i,f])
+        }
+    }
 }
 
 
-console.log(split[1][5]);
+console.log(letterX);
