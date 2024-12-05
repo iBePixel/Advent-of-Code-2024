859b3e02657f9fa76efc677a57d7ca8027ddc778
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-12-05 12:57:32 +0000

    Star 2 successful

--- Day 3/script.js
+++ Day 3/script.js
@@ -8,24 +8,62 @@ let array = [...sample.matchAll(/mul\x28[0-9]{1,3}\x2c[0-9]{1,3}\x29/igm)]
 
 let test = array.map( x => ([...x]
 ))
-console.log(test);
 
 let total = 0;
 
 const calculate = test.map((val) => {
-
   let num1 = val[0].slice(4, val[0].indexOf(','))
   let num2 = val[0].slice(val[0].indexOf(',') + 1, val[0].indexOf(')'))
 
   num1 = parseInt(num1);
   num2 = parseInt(num2)
-  console.log(num1, num2)
-  
   total += (num1 * num2);
-  //console.log(total)
+})
+
+calculate
+console.log('Star 1 answer is ', total)
+
+let total2 = 0;
+let newArr = []
+let safeStrings = []
+let safeArray = []
+
+let stops = [...sample.matchAll(/don\x27t\x28\x29/gm)]
+
+const dont = stops.map( x => [x.index]
+);
+
+let portion = sample.slice(0, dont[0]);
+safeStrings.push(portion)
+
+for(let i = 0; i < dont.length; i++){
+  let start = dont[i]
+  let stop = dont[i + 1]
+    portion = sample.slice(start, stop);
+    newArr.push(portion)
+}
+
+newArr.forEach((string) => {
+  let index = [...string.matchAll(/do\x28\x29/gm)]
+  index = index.map(x => [x.index]);
+  if(index[0] != null){
+    let temp = string.slice(index[0],)
+    safeStrings.push(temp)
+  }
+});
+
+safeStrings.forEach((string) => {
+  safeArray.push(...string.matchAll(/mul\x28[0-9]{1,3}\x2c[0-9]{1,3}\x29/igm))
 })
 
 
-calculate
+safeArray.map((val) => {
+  let num1 = val[0].slice(4, val[0].indexOf(','))
+  let num2 = val[0].slice(val[0].indexOf(',') + 1, val[0].indexOf(')'))
 
-console.log(total)
+  num1 = parseInt(num1);
+  num2 = parseInt(num2)
+  total2 += (num1 * num2);
+})
+
+console.log('Star 2 answer is ', total2)
