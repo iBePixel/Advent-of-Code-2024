24376f23651a1c5d39133205155a98bf6e1bac83
Elliott Lamb <53214529+iBePixel@users.noreply.github.com>
2024-12-08 10:38:32 +0000

    Star 2 now correct

--- Day 4/script.js
+++ Day 4/script.js
@@ -4,8 +4,8 @@ const input2 = fs.readFileSync("sample.txt")
 let sample2 = input2.toString()
 
 let sample = input.toString();
-//let split = sample.split(/\r?\n/g);
-let split = sample2.split(/\r?\n/g);
+let split = sample.split(/\r?\n/g);
+//let split = sample2.split(/\r?\n/g);
 //const includesAny = (arr, values) => values.some(v => arr.includes(v));
 
 let counter = 0;
@@ -166,109 +166,24 @@ for (let j = 0; j < letterX.length; j++) {
   downRightDiag(row, indx);
 }
 
+
+let xmasCounter = 0;
+letters = split.map(row => row.split(''))
+
+function star2() {
+  for (let i = 1; i < split.length - 1; i++){
+    for (let g = 1; g < 139; g++){
+      if (letters[i][g] === "A"){
+        if (((letters[i -1][g-1] === "M" && letters[i + 1][g+1] === "S") || (letters[i - 1][g-1] === "S" && letters[i + 1][g+1] === "M")) &&
+        ((letters[i -1][g+1] === "M" && letters[i + 1][g-1] === "S") || (letters[i -1][g+1] === "S" && letters[i + 1][g-1] === "M"))){
+          xmasCounter++
+        }
+      }
+    }
+  }
+
+}
+star2()
+
 console.log(counter);
-
-let counter2 = 0;
-let letterM = [];
-
-for (let i = 0; i < split.length; i++) {
-  let letters = split[i].split("");
-  for (let f = 0; f < 140 + 1; f++) {
-    if (letters[f] === "M") {
-      letterM.push([i, f]);
-    }
-  }
-}
-
-let checkXMasDown = function (num1, num2) {
-  row = num1;
-  upIndx1 = num2;
-  upIndx2 = num2 + 2;
-  rowMid = num1 + 1;
-  indxMid = num2 + 1;
-  lowRow = num1 + 2;
-  lowIndx1 = num2;
-  lowIndx2 = num2 + 2;
-
-  if (upIndx2 <= 139 && row <= 137 && split[row][upIndx2] === "M") {
-    if (split[rowMid][indxMid] === "A") {
-      if (split[lowRow][lowIndx1] === "M" && split[lowRow][lowIndx2] === "S") {
-        console.log('down', num1, num2);
-        counter2 += 1;
-        return true;
-      }
-    }
-  }
-  else if (upIndx2 <= 139 && row <= 137 && split[row][upIndx2] === "S") {
-    if (split[rowMid][indxMid] === "A") {
-      if (split[lowRow][lowIndx1] === "M" && split[lowRow][lowIndx2] === "S") {
-        console.log(num1, num2, 'Down - criss', num1, num2);
-        counter2 += 1;
-        return true;
-      }
-    }
-  }
-  else if (upIndx1 >= 2 && row <= 137 && split[row][num2 -2] === "S"){
-    indxMid = num2 - 1;
-    if (split[row][indxMid] === "A"){
-        lowIndx1 = num2 - 2;
-        lowIndx2 = num2;
-        if (split[lowRow][lowIndx1] === "M" && split[lowRow][lowIndx2] === "S"){
-            console.log(num1,num2, 'down - backwards', num1, num2);
-            counter2 += 1;
-            return true; 
-        }
-    }
-  }
-};
-
-let checkXMasUp = function (num1, num2) {
-    row = num1;
-    upIndx1 = num2;
-    upIndx2 = num2 + 2;
-    rowMid = num1 - 1;
-    indxMid = num2 + 1;
-    lowRow = num1 - 2;
-    lowIndx1 = num2;
-    lowIndx2 = num2 + 2;
-  
-    if (upIndx2 <= 139 && row >= 2 && split[row][upIndx2] === "M") {
-      if (split[rowMid][indxMid] === "A") {
-        if (split[lowRow][lowIndx1] === "S" && split[lowRow][lowIndx2] === "S") {
-          console.log('up', num1, num2);
-          counter2 += 1;
-          return true;
-        }
-      }
-    }
-    else if (upIndx2 <= 139 && row >= 2 && split[row][upIndx2] === "S") {
-      if (split[rowMid][indxMid] === "A") {
-        if (split[lowRow][lowIndx1] === "M" && split[lowRow][lowIndx2] === "S") {
-          console.log(num1, num2, 'up criss');
-          counter2 += 1;
-          return true;
-        }
-      }
-    }
-    else if (upIndx1 - 2 >= 2 && row >= 2 && split[row][upIndx1 - 2] === "S"){
-      indxMid = num2 - 1;
-      if (split[rowMid][indxMid] === "A"){
-          lowIndx1 = num2 - 2;
-          lowIndx2 = num2;
-          if (split[lowRow][lowIndx2] === "M" && split[lowRow][lowIndx1] === "S"){
-              console.log(num1,num2, 'up - backwards', num1, num2);
-              counter2 += 1;
-              return true; 
-          }
-      }
-    }
-  };
-
-for (let j = 0; j < letterM.length; j++) {
-  let row = letterM[j][0];
-  let indx = letterM[j][1];
-  checkXMasDown(row, indx);
-  checkXMasUp(row, indx);
-}
-
-console.log(counter2);
+console.log(xmasCounter);
