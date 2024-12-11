905b1774277d4eadeb0d7ab525cf6c1098fbd6d5
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-12-11 11:46:04 +0000

    Separating strings into pairs and arrays. To build out array sorting functionality next

--- Day 5/script.js
+++ Day 5/script.js
@@ -4,7 +4,32 @@ const sample = fs.readFileSync("sample.txt")
 
 let data = input.toString();
 data = data.trim();
+data = data.split(/\r\n/g)
+let pairs = []
+let strings = []
 
-let pairs = data.split(/\r\n/g)
+data.forEach(string => {
+    if (string.includes('|')) {
+        pairs.push(string)
+    }
+    else if (string !== '') {
+        strings.push(string)
+    }
+    
+});
 
-console.log(pairs[1200])
+pairs.forEach(pair => {
+    let split = pair.split('|')
+    let num1 = split[0]
+    let num2 = split[1]
+    strings.forEach(list => {
+        let separate = list.split(',')
+        if (list.includes(num1) && list.includes(num2)){
+            console.log(separate, num1, num2)
+            let i = separate.indexOf(num1);
+            let f = separate.indexOf(num2);
+            console.log(i, f)
+        }
+    })
+})
+
