41dde59d638c35da9ef78521f2803c22b1be7998
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-12-13 16:00:43 +0000

    Just need to isolate the duplicates in the 'visited' array so they're only counted once.

--- Day 6/script.js
+++ Day 6/script.js
@@ -7,6 +7,7 @@ let data = sample.toString()
 data = data.trim()
 data = data.split(/\n/g)
 rows = []
+let visited = []
 
 
 for( let i = 0; i < data.length; i++){
@@ -15,22 +16,91 @@ for( let i = 0; i < data.length; i++){
     row = row.split('')
     if (row.includes('^')) {
         start = [i, row.indexOf('^')]
+        i = data.length;
     }
 }
 
-function nextCell(row, index, direction){
-    if (direction === up) {
-        if(data[row -1][index] === '.'){
+let currIndex = start
+let direction = 'up'
+visited.push(currIndex)
 
+for (i = 0; i < 100; i++){
+    let r = currIndex[0];
+    let ind = currIndex[1];
+    if (direction === 'up'){
+        if (r-1 < 0){
+            console.log('Ended')
+            i = 100;
         }
-        else if( data[row -1][index] === '#'){
-            
+        else if(data[r-1][ind] !== '#'){
+            currIndex = [r -1, ind];
+            if (!visited.includes(currIndex)) {
+                
+                visited.push(currIndex);
+            }
+            console.log(currIndex)
         }
-        else {
-            console.log('End')
+        else if(data[r-1][ind] === '#'){
+            direction = 'right'
+            console.log('changing direction - right')
         }
-    } 
+    }
+    else if(direction === 'right'){
+        if (ind + 1 > 9){
+            console.log('Ended')
+            i = 100;
+        }
+        else if(data[r][ind + 1] !== '#'){
+            currIndex = [r, ind + 1];
+            if (!visited.includes(currIndex)) {
+                
+                visited.push(currIndex);
+            }
+            console.log(currIndex)
+        }
+        else if(data[r][ind + 1] === '#'){
+            direction = 'down'
+            console.log('changing direction - down')
+        }
+    }
+    else if(direction === 'down'){
+        if (r + 1 > 9){
+            console.log('Ended')
+            i = 100;
+        }
+        else if(data[r + 1][ind] !== '#'){
+            currIndex = [r +1, ind];
+            if (!visited.includes(currIndex)) {
+                
+                visited.push(currIndex);
+            }
+            console.log(currIndex)
+        }
+        else if(data[r+1][ind] === '#'){
+            direction = 'left'
+            console.log('changing direction - left')
+        }
+    }
+    else if(direction === 'left'){
+        if (ind -1 < 0){
+            console.log('Ended')
+            i = 100;
+        }
+        else if(data[r][ind -1] !== '#'){
+            currIndex = [r, ind -1];
+            if (!visited.includes(currIndex)) {
+                
+                visited.push(currIndex);
+            }
+            console.log(currIndex)
+        }
+        else if(data[r][ind -1] === '#'){
+            direction = 'up'
+            console.log('changing direction - up')
+        }
+    }
+
 }
 
-
-console.log(data[6][4]);
+console.log(visited);
+console.log(visited.length)
